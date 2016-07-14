3b18da340b7b66e599ba4380fde609fa0b836c99
cromtus <cromtus@yandex.ru>
2016-07-14 17:36:42 +0300

    Refactor (как-то так)

--- Visual.java
+++ Visual.java
@@ -1,66 +1,106 @@
-import java.io.*;
-import java.util.*;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.WindowAdapter;
-import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.universe.*;
 import javax.media.j3d.*;
 import javax.vecmath.*;
 import com.sun.j3d.utils.geometry.*;
-import java.io.IOException;
 import ours.*;
 
+@SuppressWarnings("serial")
 public class Visual extends Applet implements KeyListener {
-	static float hAngle = 0;
-	static float vAngle = 0;
-	static float scale = 1;
-	static TransformGroup tg;
-	static Transform3D transform;
-	static BranchGroup bg = new BranchGroup();
-	static Random rand = new Random();
-	static Scanner in = new Scanner(System.in);
+	private float xAngle = 0;
+	private float yAngle = 0;
+	private float scale = 1;
+	private TransformGroup tg = new TransformGroup();
+	private Transform3D transform;
+	private BranchGroup wrapper = new BranchGroup();
+	private BranchGroup everything = new BranchGroup();
+	private SimpleUniverse universe;
+	private boolean dark = false;
 	
-	public static boolean inRange(int i, int j, int k, int w, int h, int d) {
-		return i >= 0 && i < w && j >= 0 && j < h && k >= 0 && k < d;
+	final private Color3f black = new Color3f(0, 0, 0);
+	
+	public void drawMolecule(Parser p) {
+		dark = !dark;
+		wrapper.detach();
+		BranchGroup molecule = new BranchGroup();
+		int len = p.atoms.size();
+		for (int i = 0; i < len; i++) {
+			Atom atom = p.atoms.get(i);
+			Sphere sphere = new Sphere((float) (atom.radius / 20));
+			float r, g, b;
+			if (dark) {
+				/*
+				r = (float) (atom.r * 0.5);
+				g = (float) (atom.g * 0.5);
+				b = (float) (atom.b * 0.5);
+				*/
+				r = 1.f; g = 0; b = 0;
+			} else {
+				/*
+				r = (float) (1 - (1 - atom.r) * 0.5);
+				g = (float) (1 - (1 - atom.g) * 0.5);
+				b = (float) (1 - (1 - atom.b) * 0.5);
+				*/
+				r = 0; g = 1.f; b = 0;
+			}
+			Color3f color = new Color3f(r, g, b);
+			Appearance ap = new Appearance();
+			Material material = new Material(color, black, color, black, 1f);
+			ap.setMaterial(material);
+			sphere.setAppearance(ap);
+			TransformGroup cur = new TransformGroup();
+			transform = new Transform3D();
+			Vector3d vector = new Vector3d(atom.x / 20,
+										   atom.y / 20, 
+										   atom.z / 20);
+			transform.setTranslation(vector);
+			cur.setTransform(transform);
+			cur.addChild(sphere);
+			
+			BranchGroup curbg = new BranchGroup();
+			curbg.addChild(cur);
+			
+			molecule.addChild(curbg);
+		}
+		everything.addChild(molecule);
+		universe.addBranchGraph(wrapper);
 	}
 	
-	public Visual(String name) throws IOException {
-		tg = new TransformGroup();
+	public Visual() {
+		setLayout(new BorderLayout());
+		
+		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+		Canvas3D canvas = new Canvas3D(config);
+		canvas.addKeyListener(this);
+		add("Center", canvas);
+		
+		Color3f light1Color = new Color3f(1f, 1f, 1f);
+		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+		Vector3f light1Direction = new Vector3f(4.0f, -7.0f, -12.0f);
+		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
+		light1.setInfluencingBounds(bounds);
+		wrapper.addChild(light1);
+		
 		tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-		Color3f black = new Color3f(0, 0, 0);
+
+		//everything.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND | BranchGroup.ALLOW_CHILDREN_WRITE);
+		wrapper.setCapability(BranchGroup.ALLOW_DETACH);
+		tg.addChild(everything);
+		wrapper.addChild(tg);
 		
-		Parser p = new Parser(name);
-		int len = p.atoms.size();
-		System.out.print("Parsed " + len + " atoms. Enter 1 to show molecule, 2 to show grid: ");
-		
-		switch (in.next()) {
-		case "1":
-			for (int i = 0; i < len; i++) {
-				Atom atom = p.atoms.get(i);
-				Sphere sphere = new Sphere((float) (atom.radius / 20));
-				Color3f color = new Color3f(atom.r, atom.g, atom.b);
-				Appearance ap = new Appearance();
-				Material material = new Material(color, black, color, black, 1f);
-				ap.setMaterial(material);
-				sphere.setAppearance(ap);
-				TransformGroup cur = new TransformGroup();
-				transform = new Transform3D();
-				Vector3f vector = new Vector3f((float) atom.x / 20,
-											   (float) atom.y / 20, 
-											   (float) atom.z / 20);
-				transform.setTranslation(vector);
-				cur.setTransform(transform);
-				cur.addChild(sphere);
-				tg.addChild(cur);
-			}
-			break;
-		case "2":
-			int n = 50;
+		universe = new SimpleUniverse(canvas);
+		universe.getViewingPlatform().setNominalViewingTransform();
+		universe.addBranchGraph(wrapper);
+		/*
+			int n = 100;
 			double r = 1.5;
 			double max = Math.max(Math.max(p.maxx, p.maxy), p.maxz) + 2 * r;
-			Grid grid = new Grid(p, n, r, (double) max / n);
+			long t1 = System.nanoTime();
+			Grid grid = new Grid(p, n, r, max / n);
+			long t2 = System.nanoTime();
+			System.out.println((t2 - t1) / 1000000 + "ms");
 			len = grid.cells.size();
 			float scale = 0.04f;
 			for (int i = 0; i < len; i++) {
@@ -87,9 +127,9 @@ public class Visual extends Applet implements KeyListener {
 				
 				TransformGroup cur = new TransformGroup();
 				transform = new Transform3D();
-				Vector3f vector = new Vector3f((float) coords[0] * scale,
-											   (float) coords[1] * scale, 
-											   (float) coords[2] * scale);
+				Vector3d vector = new Vector3d(coords[0] * scale,
+											   coords[1] * scale, 
+											   coords[2] * scale);
 				transform.setTranslation(vector);
 				cur.setTransform(transform);
 				cur.addChild(cube);
@@ -97,47 +137,22 @@ public class Visual extends Applet implements KeyListener {
 			}
 			break;
 		}
-		bg.addChild(tg);
-		
-		Color3f light1Color = new Color3f(1f, 1f, 1f);
-		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
-		Vector3f light1Direction = new Vector3f(4.0f, -7.0f, -12.0f);
-		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
-		light1.setInfluencingBounds(bounds);
-		bg.addChild(light1);
-		
-		setLayout(new BorderLayout());
-		
-		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
-		Canvas3D canvas = new Canvas3D(config);
-		add("Center", canvas);
-		canvas.addKeyListener(this);
-		
-		SimpleUniverse universe = new SimpleUniverse(canvas);
-		universe.getViewingPlatform().setNominalViewingTransform();
-		universe.addBranchGraph(bg);
-	}
-	
-	public static void main(String[] args) throws IOException {
-		System.out.print("Name: ");
-		Visual visual = new Visual(in.next());
-		visual.addKeyListener(visual);
-		new MainFrame(visual, 600, 600); 
+		*/
 	}
 	
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
-			vAngle -= 0.03;
+			yAngle -= 0.03;
 			break;
 		case KeyEvent.VK_DOWN:
-			vAngle += 0.03;
+			yAngle += 0.03;
 			break;
 		case KeyEvent.VK_LEFT:
-			hAngle -= 0.03;
+			xAngle -= 0.03;
 			break;
 		case KeyEvent.VK_RIGHT:
-			hAngle += 0.03;
+			xAngle += 0.03;
 			break;
 		case KeyEvent.VK_EQUALS:
 			scale *= 1.2;
@@ -145,18 +160,22 @@ public class Visual extends Applet implements KeyListener {
 		case KeyEvent.VK_MINUS:
 			scale /= 1.2;
 			break;
+		case KeyEvent.VK_TAB:
+			break;
 		}
-		transform.rotX(vAngle);
+		transform.rotX(yAngle);
 		Transform3D tempTransform = new Transform3D();
-		tempTransform.rotY(hAngle);
+		tempTransform.rotY(xAngle);
 		transform.mul(tempTransform);
 		tempTransform = new Transform3D();
 		tempTransform.setScale(scale);
 		transform.mul(tempTransform);
 		tg.setTransform(transform);
 	}
+	
 	public void keyReleased(KeyEvent e){
 	}
+	
 	public void keyTyped(KeyEvent e){
 	}
 }
