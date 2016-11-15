652d226663ba65a0ba33cddaf9d9f28f51284f24
Ухатов <cromtus@yandex.ru>
2016-11-15 13:12:00 +0300

    Нашёлся заряд

--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
@@ -15,27 +16,57 @@ public class Parser {
 		int len = original.atoms.size();
 		for (int i = 0; i < len; i++) {
 			Atom atom = original.atoms.get(i);
-			atoms.add(new Atom(atom.name, atom.x, atom.y, atom.z));
+			atoms.add(new Atom(atom.name, atom.x, atom.y, atom.z, atom.charge));
+		}
+	}
+	
+	public double findCharge(String s){
+		double charge = 0;
+		String residue = s.substring(17, 20).trim();
+		String position = s.substring(14, 16).trim();
+		String cha = s.substring(78, 80).trim();		
+		String name = s.substring(76, 78).trim();
+		if (!cha.equals("")){
+			return Double.parseDouble(cha); 
+		}else{
+			switch (name) {
+			case "O":
+				charge = -0.5;
+				if (position.equals("")){
+					charge = -1;
+				}
+				return charge;
+			case "N":
+				charge = 0.5;
+				if ((residue.equals("LYS") && position.substring(14,15).equals("Z")) || position.equals("")){
+					charge = 1;
+				}
+				if ( (residue.equals("PRO"))){
+					charge = -0.1;
+				}
+				return charge;
+			default:
+				return charge;
+			}
 		}
 	}
 
 	public Parser(File file) throws FileNotFoundException {
 		atoms = new ArrayList<>();
-		Locale.setDefault(Locale.US);
 		Scanner in = new Scanner(file);
 		String s = in.nextLine();
-		double maxR = 0;
 		while (!s.substring(0, 3).equals("END")) {
-			if (s.substring(0, 4).equals("ATOM")) {
+			if (s.substring(0, 4).equals("ATOM") || s.substring(0, 6).equals("HETATM")) {
+				System.out.println(s.substring(0, 4));
 				char c = s.charAt(10);
 				if (c >= '0' && c <= '9') {
 					double x = Double.parseDouble(s.substring(30, 38).trim());
 					double y = Double.parseDouble(s.substring(38, 46).trim());
 					double z = Double.parseDouble(s.substring(46, 54).trim());
 					String nm = s.substring(76, 78).trim();
-					Atom atom = new Atom(nm, x, y, z);
+					double charge = findCharge(s);
+					Atom atom = new Atom(nm, x, y, z, charge);
 					double r = atom.radius;
-					maxR = Math.max(maxR, r);
 					atoms.add(atom);
 
 					minX = Math.min(minX, x - r);
@@ -57,7 +88,7 @@ public class Parser {
 			Atom atom = atoms.get(i);
 			size = Math.max(size, ((cx-atom.x)*(cx-atom.x) + (cy-atom.y)*(cy-atom.y)+(cz-atom.z)*(cz-atom.z)));
 		}
-		size = Math.sqrt(size) + maxR;
+		size = Math.sqrt(size);
 		for (int i = 0; i < len; i++) {
 			Atom atom = atoms.get(i);
 			atom.x -= cx-size;
@@ -73,7 +104,8 @@ public class Parser {
 
 
 	public double getSize() {
-		return size*2;
+		return size * 2;
 	}
 }
+
 	
